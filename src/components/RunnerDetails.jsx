4a281c1b50f494072c2f2349e6bac1b82297014a
Trlaic <igortrlajic@gmail.com>
2024-01-23 22:27:45 +0100

    x05: RunnerDetails component styling in progress

--- src/components/RunnerDetails.jsx
+++ src/components/RunnerDetails.jsx
@@ -4,15 +4,22 @@ import styles from './RunnerDetails.module.scss';
 export default function RunnerDetails() {
     return (
         <div className={styles.container}>
-            <h1>Runner details</h1>
-            <p>You can add max 10 participants</p>
-            <form>
-                <label htmlFor="runner-name">Name</label>
-                <input type="text" id="runner-name" />
-                <label htmlFor="runner-speed">Speed</label>
-                <input type="number" id="runner-speed" />
-                <label htmlFor="runner-start-time">Start time</label>
-                <input type="time" id="runner-start-time" />
+            <h1 className={styles.heading}>Runner details</h1>
+            <p className={styles.paragraph}>*You can add max 10 participants</p>
+            <form className='form'>
+                <div>
+                    <label className={styles.label} htmlFor="runner-name">Name</label>
+                    <input className={styles.input} type="text" id="runner-name" placeholder="Usain Bolt"/>
+                </div>
+                <div>
+                    <label className={styles.label} htmlFor="runner-speed">Speed</label>
+                    <input className={styles.input} type="number" id="runner-speed" placeholder='10 km/h'/>
+                </div>
+                <div>
+                    <label className={styles.label} htmlFor="runner-start-time">Start time</label>
+                    <input className={styles.input} type="time" id="runner-start-time"/>
+                </div>
+                <button>+ Add Runner</button>
             </form>
         </div>
     )
